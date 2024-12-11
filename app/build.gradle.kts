41f99cb6c50d34b3624c7396d8ec52540ceb8e92
Sean Smith <sjsmith5@student.fullsail.edu>
2024-12-11 11:26:37 -0800

    Fix plugin configuration and apply Google Services plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
+
+apply(plugin = "com.google.gms.google-services")
