e45d003d48e9ebcd59cb2b14056a64564881b8c1
Sean Smith <sjsmith5@student.fullsail.edu>
2024-12-12 06:48:53 -0800

    Updated configuration and other project files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -51,4 +52,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Firebase SDKs
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation("com.google.firebase:firebase-analytics:22.1.2")
 }
+
+    // Apply Google Services Plugin
+    apply(plugin = "com.google.gms.google-services")
