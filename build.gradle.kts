7de60b583cfd8704ca4e8fa5b87e0ac639316e41
Sean Smith <sjsmith5@student.fullsail.edu>
2024-12-12 06:47:30 -0800

    Added Gradle Build Scan terms of use agreement in the build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,21 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.7.3")
+        classpath("com.google.gms:google-services:4.4.2")
+    }
+}
+
+allprojects {
+//no blocked repos
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+    }
+}
